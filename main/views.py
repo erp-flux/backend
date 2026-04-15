fb644cf6d007dd2b9dced71bef4060e6caf623b7
Bignon Boris <bignonboris@gmail.com>
2026-04-16 00:55:04 +0100

    set up backend

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("<h1>Salut ! Ton projet Django est prêt.</h1>")
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
